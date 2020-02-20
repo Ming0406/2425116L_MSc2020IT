ee2c25fd628aaa6b11d31bf16b4c509c6221488e
Ming0406 <Backwite@hotmail.com>
2020-02-20 05:05:11 +0000

    Chapter 7

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,7 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
+    NAME_MAX_LENGTH = 200
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
